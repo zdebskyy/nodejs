6c9cc7fa510e18588e96f97df93dfd44f2189341
Oleksandr Zdebskyi <zdebskyi@MacBook-Air-Oleksandr.local>
2021-01-25 20:39:24 +0200

    some

--- avatar/avatarCreater.js
+++ avatar/avatarCreater.js
@@ -5,18 +5,13 @@ const path = require("path");
 
 async function avatarCreate() {
   const avatar = Avatar.squareBuilder(128);
-
   const avatarIcon = await avatar.create("gabriel");
-
   const avatarName = `${Date.now()}.png`;
-
   const avatarPath = path.join("tmp", avatarName);
   const finalPath = path.join("public", "images", avatarName);
 
   await fs.writeFile(avatarPath, avatarIcon);
-
   await fs.copyFile(avatarPath, finalPath);
-
   await fs.unlink(avatarPath);
 
   return avatarName;
